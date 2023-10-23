b72f1b58a2e129b4e69704f3f91c6de57ef67a39
Alex Summers <82482842+AyeitsAlex21@users.noreply.github.com>
2023-10-23 11:50:50 -0700

    Update library.py

--- library.py
+++ library.py
@@ -272,5 +272,24 @@ class CustomRobustTransformer(BaseEstimator, TransformerMixin):
     self.fit(X, y)
     return self.transform(X)
 
+def find_random_state(features_df, labels, n=200):
+  var = []  #collect test_error/train_error where error based on F1 score
+
+
+  for i in range(1, n):
+    train_X, test_X, train_y, test_y = train_test_split(features_df, labels, test_size=0.2, shuffle=True,
+                                                    random_state=i, stratify=labels)
+    model.fit(train_X, train_y)  #train model
+    train_pred = model.predict(train_X)           #predict against training set
+    test_pred = model.predict(test_X)             #predict against test set
+    train_f1 = f1_score(train_y, train_pred)   #F1 on training predictions
+    test_f1 = f1_score(test_y, test_pred)      #F1 on test predictions
+    f1_ratio = test_f1/train_f1          #take the ratio
+    var.append(f1_ratio)
+    
+  rs_value = sum(var)/len(var)
+  
+  return np.array(abs(var - rs_value)).argmin()
+
 
 
